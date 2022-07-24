1f24e36e63e14c8b263908e3924ffe6421eac5dc
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-07-24 09:47:20 +0300

    custom hook

--- src/components/ArticlePage.js
+++ src/components/ArticlePage.js
@@ -1,34 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { makeEmojiList } from "../utils";
+import useDocumentTitle from "../hooks/useDocumentTitle";
 
 function ArticlePage() {
   // fetch data for a post
-  const { id } = useParams();
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [post, setPost] = useState(null);
-
-  const url = `http://localhost:4000/posts/${id}`;
-  useEffect(() => {
-    setIsLoaded(false);
-    fetch(url)
-      .then((r) => r.json())
-      .then((post) => {
-        setPost(post);
-        setIsLoaded(true);
-      });
-  }, [url]);
+  function ArticlePage() {
+    const { id } = useParams();
+    const { data: post, isLoaded } = useQuery(
+      `http://localhost:4000/posts/${id}`
+    );
 
   // set the document title
   const pageTitle = post ? `Underreacted | ${post.title}` : "Underreacted";
-  useEffect(() => {
-    document.title = pageTitle;
-  }, [pageTitle]);
+  useDocumentTitle(pageTitle);
 
   if (!isLoaded) return <h3>Loading...</h3>;
 
   const { minutes, title, date, preview } = post;
-  const emojis = makeEmojiList(minutes);
+  const emojis = makeEmojiList(minutes)};
 
   return (
     <article>
@@ -41,4 +31,4 @@ function ArticlePage() {
   );
 }
 
-export default ArticlePage;
+export default ArticlePage
